965c256abc7883b68344c6757d5fc9ffb5662524
Lalit Maganti <lalitmaganti@gmail.com>
2013-09-16 21:13:16 +0200

    Merge branch 'korg/linux-3.4.y' of git://codeaurora.org/quic/la/kernel/msm into cm-10.2-next
    * kang d2 net/bluetooth to fix conflicts
    
    Conflicts:
    	arch/arm/Kconfig
    	arch/arm/include/asm/mutex.h
    	arch/arm/kernel/traps.c
    	arch/arm/mm/dma-mapping.c
    	arch/arm/mm/tlb-v7.S
    	arch/arm/vfp/vfpmodule.c
    	drivers/base/power/main.c
    	drivers/bluetooth/ath3k.c
    	drivers/bluetooth/btusb.c
    	drivers/gpu/drm/radeon/radeon_mode.h
    	drivers/mmc/card/block.c
    	drivers/mmc/host/sdhci.c
    	drivers/net/tun.c
    	drivers/usb/core/hub.c
    	drivers/usb/core/message.c
    	drivers/usb/host/xhci.h
    	fs/ubifs/dir.c
    	include/linux/freezer.h
    	include/linux/sched.h
    	include/net/bluetooth/hci_core.h
    	include/net/bluetooth/mgmt.h
    	kernel/cgroup.c
    	kernel/futex.c
    	kernel/power/suspend.c
    	kernel/signal.c
    	mm/madvise.c
    	net/bluetooth/hci_conn.c
    	net/bluetooth/hci_core.c
    	net/bluetooth/hci_event.c
    	net/bluetooth/l2cap_core.c
    	net/bluetooth/mgmt.c
    	net/bluetooth/rfcomm/sock.c
    	net/bluetooth/smp.c
    
    Change-Id: Ic5dff9d68aabd27bb2698ae8f78fc7fd855b8faf

--- include/net/bluetooth/l2cap.h
+++ include/net/bluetooth/l2cap.h
@@ -1,6 +1,6 @@
 /*
    BlueZ - Bluetooth protocol stack for Linux
-   Copyright (c) 2000-2001, 2010-2012 Code Aurora Forum.  All rights reserved.
+   Copyright (c) 2000-2001, 2010-2012 The Linux Foundation.  All rights reserved.
    Copyright (C) 2009-2010 Gustavo F. Padovan <gustavo@padovan.org>
    Copyright (C) 2010 Google Inc.
 
@@ -442,6 +442,7 @@ struct l2cap_conn {
 	__u8		info_ident;
 
 	struct timer_list info_timer;
+	struct smp_chan *smp_chan;
 
 	spinlock_t	lock;
 
@@ -717,5 +718,7 @@ void l2cap_amp_logical_complete(int result, struct hci_conn *ampcon,
 				struct hci_chan *ampchan, struct sock *sk);
 
 void l2cap_amp_logical_destroyed(struct hci_conn *ampcon);
+
+void l2cap_conn_del(struct hci_conn *hcon, int err, u8 is_process);
 
 #endif /* __L2CAP_H */
